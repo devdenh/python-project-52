d9a7f8e3dc9821a27c4bbca7e2766f58fd86e986
devdenh <denisaminov74@gmail.com>
2022-11-15 15:45:56 +0500

    change db to sqlite

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -88,7 +88,6 @@ DATABASE_URL = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
 DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
