5200793befa7ea2b1aa15ab4b2fe1efdaf12cca4
devdenh <denisaminov74@gmail.com>
2022-11-15 14:41:55 +0500

    step3 update settings engine

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -90,7 +90,7 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 # 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'))
 
 
-DATABASES = {'default': dj_database_url.config(default=DATABASE_URL)}
+DATABASES = {'default': dj_database_url.config(default=DATABASE_URL, engine="django.db.backends.postgresql")}
 
 
 # Password validation
