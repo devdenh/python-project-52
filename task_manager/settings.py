a8da95fad4e39a2c4ffe1aeb6abd0c14891bc89c
devdenh <denisaminov74@gmail.com>
2022-11-26 17:10:32 +0500

    step8: trying new settings for db

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
-import rollbar
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -72,8 +71,6 @@ ROLLBAR = {
     'code_version': '1.0',
     'root': BASE_DIR,
 }
-
-rollbar.init(**ROLLBAR)
 
 ROOT_URLCONF = "task_manager.urls"
 
